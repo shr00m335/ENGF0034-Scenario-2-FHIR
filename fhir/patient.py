2088480d9b8a70d3310f78f6e0a9ce3a6529884b
OrdinaryMushroom <ordinarymushroom335@gmail.com>
2024-03-03 17:13:09 +0000

    Added gender, birth date and telecom

--- fhir/patient.py
+++ fhir/patient.py
@@ -4,6 +4,9 @@ class Patient:
     def __init__(self, data: Dict):
         self._family_name: str = data["name"][0]["family"]
         self._given_name: List[str] = data["name"][0]["given"]
+        self._gender: str = data["gender"]
+        self._birth_date = data["birthDate"]
+        self._telecom = data["telecom"]
 
     def __repr__(self) -> str:
         return f"<Patient name='{self._given_name[0]} {self._family_name}'>"
