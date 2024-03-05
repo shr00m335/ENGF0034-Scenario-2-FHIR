d4fb8b392b04f05a42e6710fccfaf6f1c3c72e4a
shr00m335 <samson.wulongtin@gmail.com>
2024-03-05 15:45:35 +0000

    Added address and mr to Patient class

--- fhir/patient.py
+++ fhir/patient.py
@@ -8,6 +8,8 @@ class Patient:
         self._gender: str = data["gender"]
         self._birth_date = data["birthDate"]
         self._telecom = data["telecom"]
+        self._address = data[""]
+        self._mediacal_record_number = data[""]
 
     def __repr__(self) -> str:
         return f"<Patient name='{self._given_name[0]} {self._family_name}'>"
