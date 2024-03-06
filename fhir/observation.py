45ca92e588ea12263425e6fdd1340abf22c6fadb
shr00m335 <samson.wulongtin@gmail.com>
2024-03-06 09:37:05 +0000

    Added ability to read observation with multiple values and only text value

--- fhir/observation.py
+++ fhir/observation.py
@@ -3,8 +3,25 @@ class Observation:
         self._code = data["code"]["coding"][0]["code"]
         self._text = data["code"]["text"]
         self._issued_time = data["issued"]
-        self._value = data["valueQuantity"]["value"]
-        self._unit = data["valueQuantity"]["unit"]
+        if "component" in data:
+            self._value = [x["valueQuantity"]["value"] for x in data["component"]]
+            self._unit = data["component"][0]["valueQuantity"]["unit"]
+        elif "valueQuantity" in data:
+            self._value = [data["valueQuantity"]["value"]]
+            self._unit = data["valueQuantity"]["unit"]
+        else:
+            print(data)
+            self._value = data["valueCodeableConcept"]["text"]
+            self._unit = None
+        
 
     def __repr__(self) -> str:
         return f"<Observation code={self._code} name=\"{self._text}\" value={self._value}>"
+    
+    def to_json(self):
+        return {
+            "item": self._text,
+            "value": self._value,
+            "unit": self._unit,
+            "time": self._issued_time
+        }
