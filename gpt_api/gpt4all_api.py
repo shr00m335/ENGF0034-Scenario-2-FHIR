b9f6574d0faa2e6d3ce62abdaa9d53290b0b58a9
OrdinaryMushroom <ordinarymushroom335@gmail.com>
2024-03-08 16:59:42 +0000

    Changed running hardware to CPU

--- gpt_api/gpt4all_api.py
+++ gpt_api/gpt4all_api.py
@@ -5,7 +5,7 @@ from .llm_api import LLM_Api
 class GPT4All_Api(LLM_Api):
     def __init__(self):
         super().__init__()
-        self._model = GPT4All("mistral-7b-openorca.gguf2.Q4_0.gguf", device="gpu")
+        self._model = GPT4All("mistral-7b-openorca.gguf2.Q4_0.gguf", device="cpu")
         self._prompt_template = 'USER: {0}\nASSISTANT: '
 
     def get_response(self, prompt: str):
